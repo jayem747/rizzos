f19220de66599b3c27a76a2ddc22fb18dae59505
jay <junemassa@gmail.com>
2023-06-29 12:16:51 +0200

    edited sign in

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rizzo's</title>
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
-    <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body style="text-align: center;">
     
@@ -27,11 +25,8 @@
                 <a class="nav-link" href="#"><img src="assets/shopping.png" width="35" height="auto"></a>
             </li>
             <li class="nav-item">
-    <a class="nav-link" href="#" data-toggle="modal" data-target="#userModal">
-        <img src="assets/user.png" width="26" height="auto">
-    </a>
-</li>
-
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#userModal"><img src="assets/user.png" width="26" height="auto"></a>
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="index.php"><img src="assets/home.png" width="32" height="auto"></a>
             </li>
@@ -57,7 +52,7 @@
   <a href="pizza_shop.php" style="display: block; margin-bottom: 50px;">
     <img src="assets/aanbiedingen/aanbieding2.jpg" style="margin: 0 auto;" />
   </a>
-  <a href="https://example.com/link3" style="display: block;">
+  <a href="drinks_shop.php" style="display: block;">
     <img src="assets/aanbiedingen/aanbieding3.jpg" style="margin: 0 auto;" />
   </a>
 
@@ -66,23 +61,41 @@
     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="userModalLabel">Sign In / Sign Up</h5>
+            <div class="modal-header border-0">
+						<h5>Sign In</h5>
+						<h5>Sign Up</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <!-- Add your sign-in / sign-up form here -->
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <!-- Login and Registration forms -->
+                <div class="row">
+                    <div class="col-md-6"> 
+                        <form method="POST" action="login_sign_up.php">
+                            <input type="text" name="username" placeholder="Username" required>
+                            <input type="password" name="password" placeholder="Password" required>
+                            <button type="submit" name="login">Login</button>
+                        </form>
+                    </div>
+                    <div class="col-md-6">
+                        <form method="POST" action="login_sign_up.php">
+                            <input type="text" name="username" placeholder="Username" required>
+                            <input type="password" name="password" placeholder="Password" required>
+                            <button type="submit" name="register">Register</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 <!-- End Modal for sign in / sign up -->
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
 
